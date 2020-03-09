c345bd6838d49426721ff4db3669dc7e23570aaf
seakjh <skykjh0513@gmail.com>
2020-03-09 16:02:12 +0900

    문제 배열 생성 + 숫자 입력까지

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -1,8 +1,20 @@
 package code;
 
+import java.util.Scanner;
+
 public class MainDrive {
 	
 	public static void main(String[] args) {
+		
+//		정답은 471로 강제설정. => 배열로 만들면 좀 더 편함.
+		int [] answerArr = {4, 7, 1};
+		
+//		사용자에게 3자리숫자 입력 => 배열로 분리.
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("세자리 숫자로 정답 입력 : ");
+		int userInput = scan.nextInt();
+		
 		
 	}
 
