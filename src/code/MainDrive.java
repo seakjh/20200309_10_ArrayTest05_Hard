130f7ffc6fab0356b9fcee3226befb943694efdb
seakjh <skykjh0513@gmail.com>
2020-03-09 16:09:08 +0900

    입력 받은 숫자를 세칸의 배열로 분리
    100의자리 : /100 으로
    10의 자리 : /10 %10 으로
    1의 자리 : %10 으로

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -13,7 +13,21 @@ public class MainDrive {
 		
 		Scanner scan = new Scanner(System.in);
 		System.out.println("세자리 숫자로 정답 입력 : ");
-		int userInput = scan.nextInt();
+		int num = scan.nextInt();
+		
+//		입력받은 세자리 숫자를 배열로 분리
+//		123 => {1,2,3} 배열로.
+		
+		int[] userInputArr = new int[3];
+//		0번칸 => 입력한 숫자의 100의 자리값.
+//		456입력 => 4 => 100으로 나눈 몫.
+		userInputArr[0] = num / 100;
+//		1번칸 => 입력한 숫자의 10의 자리값.
+//		456입력 => 5 => 456 => 45 => 5
+		userInputArr[1] = num / 10 % 10;
+//		2번칸 => 입력한 숫자의 1의 자리값.
+//		456 입력 => 6 => 10으로 나눈 나머지.
+		userInputArr[2] = num % 10;
 		
 		
 	}
