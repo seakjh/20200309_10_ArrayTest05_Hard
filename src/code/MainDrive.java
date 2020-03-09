f25751b0159644a47db2b863d77bbad822e20da1
seakjh <skykjh0513@gmail.com>
2020-03-09 16:43:15 +0900

    입력값의 S/B 갯수 판단 출력 + 정답 입력을 무한반복
    - 남은이슈 : 3S면 반복 종료.

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -12,23 +12,72 @@ public class MainDrive {
 //		사용자에게 3자리숫자 입력 => 배열로 분리.
 		
 		Scanner scan = new Scanner(System.in);
-		System.out.println("세자리 숫자로 정답 입력 : ");
-		int num = scan.nextInt();
 		
-//		입력받은 세자리 숫자를 배열로 분리
-//		123 => {1,2,3} 배열로.
+//		한번 입력받고 끝이 아니라 정답을 맞을때까지 반복 입력.
+//		반복 횟수가 불명확한 반복 => while(true) 활용
 		
-		int[] userInputArr = new int[3];
-//		0번칸 => 입력한 숫자의 100의 자리값.
-//		456입력 => 4 => 100으로 나눈 몫.
-		userInputArr[0] = num / 100;
-//		1번칸 => 입력한 숫자의 10의 자리값.
-//		456입력 => 5 => 456 => 45 => 5
-		userInputArr[1] = num / 10 % 10;
-//		2번칸 => 입력한 숫자의 1의 자리값.
-//		456 입력 => 6 => 10으로 나눈 나머지.
-		userInputArr[2] = num % 10;
+		while (true) {
+			System.out.print("세자리 숫자로 정답 입력 : ");
+			int num = scan.nextInt();
+			
+//			입력받은 세자리 숫자를 배열로 분리
+//			123 => {1,2,3} 배열로.
+			
+			int[] userInputArr = new int[3];
+//			0번칸 => 입력한 숫자의 100의 자리값.
+//			456입력 => 4 => 100으로 나눈 몫.
+			userInputArr[0] = num / 100;
+//			1번칸 => 입력한 숫자의 10의 자리값.
+//			456입력 => 5 => 456 => 45 => 5
+			userInputArr[1] = num / 10 % 10;
+//			2번칸 => 입력한 숫자의 1의 자리값.
+//			456 입력 => 6 => 10으로 나눈 나머지.
+			userInputArr[2] = num % 10;
+			
+//			문제의 입력이 전부 배열로 분리됨.
+//			=> ?s ?b인지 판단.
+//			=> 스트라이크의 개수 / 볼 개수를 저장할 변수가 필요하다.
+			int strikeCount = 0;
+			int ballCount = 0;
+			
+//			입력과 문제를 비교해서 스트라이크 볼 갯수 카운팅
+			for (int i = 0; i < userInputArr.length; i++) {
+				for (int j = 0; j < answerArr.length; j++) {
+					
+//					내가 입력한 i번째 숫자와
+//					정답의 j번째 숫자가 같은가?
+//					일단 숫자가 같아야 s/b이건 판단할 가치가 있다.
+					
+					if (userInputArr[i] == answerArr[j]) {
+						
+//						추가검사. 값이 같을때. 위치도 같은가?
+//						위치가 같다면? 스트라이크 찾음.
+//						위치가 다르면? 볼 찾음.
+						
+						if (i == j) {
+//							숫자도 같고 위치도 같은상황 => S
+//							S 한개 더 찾았다. 증가
+							strikeCount++;
+						}
+						else {
+//							숫자가 같은데 위치가 다름 => B 한개 더 찾음.
+							ballCount++;
+						}
+						
+					}
+					
+				}
+			}
+			
+//			?s ?b인지 계산이 끝난
+//			화면에 결과 출력.
+			
+			System.out.println(strikeCount + "S" + ballCount + "B 입니다.");
+			
+		}
 		
+		
+
 		
 	}
 
